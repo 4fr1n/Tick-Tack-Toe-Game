a31e6e8bc3c42380f6a75b71ca8301bca424bd32
4fr1n <raj.entropy@gmail.com>
2024-12-05 08:22:49 +0530

    AI-Added

--- game.py
+++ game.py
@@ -19,10 +19,7 @@ for i in range(1, 10):
 
 turn = "x"
 
-def checkResult():
-    global board
-
-    winning_combinations = [
+winning_combinations = [
         (1, 2, 3),  
         (4, 5, 6),  
         (7, 8, 9),  
@@ -31,9 +28,10 @@ def checkResult():
         (3, 6, 9),  
         (1, 5, 9),  
         (3, 5, 7),  
-    ]
-    
-    
+ ]
+
+def checkResult():
+    global board   
     for combo in winning_combinations:
         if board[combo[0]] == board[combo[1]] == board[combo[2]] and board[combo[0]] != "":
             messagebox.showinfo("Game Over", f"Player '{board[combo[0]]}' wins!")
@@ -54,6 +52,62 @@ def resetGame():
     for button in frame2.winfo_children():
         button["text"] = " "
 
+def minimax(Maximizing):
+    global board
+    for combo in winning_combinations:
+        if board[combo[0]]== board[combo[1]] == board[combo[2]]:
+            if board[combo[0]]=="x":
+                return 1
+            elif board[combo[0]]=="o":
+                return -1
+    if all (board[pos]!="" for pos in board):
+        return 0
+    
+    if Maximizing:
+        best_score = -float("inf")
+        for pos in board:
+            if board[pos] == "":
+                board[pos] = "x"
+                score = minimax(False)
+                board[pos] = ""  
+                best_score = max(best_score, score)
+        return best_score
+    else:
+        best_score = float("inf")
+        for pos in board:
+            if board[pos] == "":
+                board[pos] = "o"
+                score = minimax(True)
+                board[pos] = ""  
+                best_score = min(best_score, score)
+        return best_score
+
+def BestMove():
+    global board
+    best_score = float("inf")  
+    best_move = None
+    for pos in board:
+        if board[pos] == "":
+            board[pos] = "o"
+            score = minimax(True)
+            board[pos] = ""  
+            if score < best_score:
+                best_score = score
+                best_move = pos
+    return best_move
+
+def botPlay():
+    global turn
+    if turn == "o":
+        ai_move = BestMove()
+        if ai_move is not None:
+            for button in frame2.winfo_children():
+                if button._name == f"button{ai_move}":
+                    button["text"] = "O"
+                    break
+            board[ai_move] = "o"
+            turn = "x"
+            checkResult()
 
 def play(event):
     global turn
@@ -65,11 +119,10 @@ def play(event):
             button["text"] = "X"
             turn = "o"
             board[clicked] = "x"
+            checkResult()
         else:
-            button["text"] = "O"
-            turn = "x"
-            board[clicked] = "o"
-    checkResult()
+            botPlay()
+    
 
 
 for i in range(3):
